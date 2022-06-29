01d876e030cf35406d6b9970f1744b5194d4514a
Wellington <damasiowellington7@gmail.com>
2022-06-29 17:37:24 -0300

    Melhorando arquivo .js de animação

--- animations.js
+++ animations.js
@@ -2,4 +2,13 @@ function name(a, b, c) {
   return a - b - c
 }
 
+function animate(animName, animDuration, animAction) {
+  return {
+    lang: 'css',
+    name: 'bouncing',
+    duration: 22,
+    action: 'bounce'
+  }
+}
+
 module.exports = name
